ca21546739b2727f6a63d85f5dcfbaa61f94d805
Gabe Kangas <gabek@real-ity.com>
2020-02-18 21:33:27 -0800

    Glow + Flicker extension

--- assistant/extensions/UIView+Glow.swift
+++ assistant/extensions/UIView+Glow.swift
@@ -11,8 +11,30 @@ import Foundation
 extension UIView {
     func enableGlow(with color: UIColor) {
         layer.shadowOpacity = 1.0
-        layer.shadowOffset = .zero//CGSize(width: 1, height: 1) //.zero
+        layer.shadowOffset = .zero
         layer.shadowRadius = 10.0
         layer.shadowColor = color.cgColor
+        
+        let animation = CABasicAnimation(keyPath: "shadowOpacity")
+        animation.fromValue = 1.0
+        animation.toValue = 0.7
+        animation.repeatCount = .infinity
+        animation.duration = 1.0
+        animation.autoreverses = true
+        animation.isRemovedOnCompletion = true
+        animation.fillMode = CAMediaTimingFillMode.forwards
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        layer.add(animation, forKey: "glowViewPulseAnimation")
+        
+        let radiusAnimation = CABasicAnimation(keyPath: "shadowRadius")
+        radiusAnimation.fromValue = 10.0
+        radiusAnimation.toValue = 5.0
+        radiusAnimation.repeatCount = .infinity
+        radiusAnimation.duration = 2.5
+        radiusAnimation.autoreverses = true
+        radiusAnimation.isRemovedOnCompletion = true
+        radiusAnimation.fillMode = CAMediaTimingFillMode.forwards
+        radiusAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        layer.add(radiusAnimation, forKey: "glowViewPulseRadiusAnimation")
     }
 }
